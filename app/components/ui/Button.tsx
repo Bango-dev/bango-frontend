1f0d6ffc345c4f7f6ad04f6bb3a3b96c88ed95dd
andervies <anderclace@gmail.com>
2025-11-21 15:10:27 +0100

    Add alignment styling

--- app/components/ui/Button.tsx
+++ app/components/ui/Button.tsx
@@ -38,7 +38,7 @@ const Button = ({
   return (
     <div className={`btns ${className}`}>
       <button
-        className="btn-primary w-full flex justify-center items-center text-center"
+        className="btn-primary flex justify-center items-center text-center"
         onClick={handleFirstClick}
         type={type}
       >
